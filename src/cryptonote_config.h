4b1a56817732accbf63d176d25ea6b0f878b03ab
MancyNancy <github@reallymymail.com>
2014-07-10 20:59:01 -0700

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -5,8 +5,8 @@
 #pragma once
 
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     13004500 	
-#define CRYPTONOTE_MAX_BLOCK_SIZE                       500000000  // block header blob limit, never used!
-#define CRYPTONOTE_MAX_TX_SIZE                          1000000000
+#define CRYPTONOTE_MAX_BLOCK_SIZE                       50000  // block header blob limit, never used!
+#define CRYPTONOTE_MAX_TX_SIZE                          12345678
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
 //TODO Currency-specific address prefix
 #define CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX        
@@ -26,18 +26,18 @@
 #define EMISSION_SPEED_FACTOR                           (18)
 
 //TODO Define number of blocks for block size median calculation
-#define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 100
+#define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 50
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE       25 //size of block (bytes) after which reward for block calculated using block size
-#define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
+#define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          100
 //TODO Define number of digits
-#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                
+#define CRYPTONOTE_DISPLAY_DECIMAL_POINT                8
 //TODO Define number of smallest units in one coin
-#define COIN                                            
+#define COIN                                            0.000000001
 //TODO Define default fee for transactions
-#define DEFAULT_FEE                                     
+#define DEFAULT_FEE                                     .000125
 
 //TODO Define preferred block's target time
-#define DIFFICULTY_TARGET                               120 // seconds
+#define DIFFICULTY_TARGET                               667 // seconds
 //TODO There are options to tune CryptoNote's difficulty retargeting function. 
 //TODO We recommend not to change it.
 #define DIFFICULTY_WINDOW                               720 // blocks
